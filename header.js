d21edd2b78f2b74db72de6394151c76ee728dd62
Nick <61029188+nickadaniel@users.noreply.github.com>
2022-02-18 10:56:51 -0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+//left hand side for the logo front end page part 
+// search icon for the right hand side 
+//container for div 
+// backend dev 
+//functions // 
+//install the ui part for the icons 
+//search icon  
+
+
+function Header() {
+    //image logo 
+    <div classname='header'>
+        <img 
+        classname="C:\Users\anoth\OneDrive\Desktop\amazon\images\ref=nav_logo.html"
+        />
+
+
+    <div classname="header__nav">
+        <div classname='header__option'>
+            <span 
+            classname='header_optionlineon'>
+                Hello Guest
+            </span>
+            <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+
+            <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        
+          <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+       
+            <span 
+            classname='header__optionLineTw'>
+                classname='header__optionLineTw'
+            </span>
+        </div>
+</div>
+</div>
+
+}
+
+// class name 
+//
